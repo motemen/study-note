93e3d74cbc4d567608ceac18e6e774b14509355c
motemen <motemen@gmail.com>
2014-07-02 20:07:19 +0900

    init

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem "middleman", "~>3.3.3"
+
+gem "middleman-livereload", "~> 3.1.0"
+
+gem 'asciidoctor', github: 'asciidoctor'
+gem 'slim'
+gem 'bootstrap-sass', require: false
---
+++ config.rb
@@ -0,0 +1,11 @@
+configure :development do
+  activate :livereload
+end
+
+set :css_dir,    'stylesheets'
+set :js_dir,     'javascripts'
+set :images_dir, 'images'
+
+config[:file_watcher_ignore] << /\.sw.$/
+
+set :asciidoc_attributes, %w(sectanchors)
